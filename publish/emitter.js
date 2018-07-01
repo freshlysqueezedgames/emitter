6113035f88a229562732f9ec4d08d766c26656d9
occammedia <carl.p@freshly-squeezed.com>
2018-07-01 21:23:18 +0100

    Built solution

--- publish/emitter.js
+++ publish/emitter.js
@@ -230,6 +230,25 @@ exports.default = Emitter;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.Message = undefined;
+
+var _Emitter = require('./Emitter');
+
+var _Emitter2 = _interopRequireDefault(_Emitter);
+
+var _Message = require('./Message');
+
+var _Message2 = _interopRequireDefault(_Message);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.Message = _Message2.default;
+exports.default = _Emitter2.default;
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
